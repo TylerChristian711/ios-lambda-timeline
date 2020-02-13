46e02f4a3c2ef846a6daee85f91ccd324ffd4aa4
Tyler Christian <tylerc71197@gmail.com>
2020-02-13 16:40:33 -0500

    refactored the videoPost to allow geoTags

--- LambdaTimeline/Models/VideoPost.swift
+++ LambdaTimeline/Models/VideoPost.swift
@@ -9,35 +9,41 @@
 import Foundation
 import FirebaseAuth
 
-class VideoPost {
+class VideoPost: NSObject {
     
-       var mediaURL: URL
-       let mediaType: MediaType
-       let author: Author
-       let timestamp: Date
-       var comments: [Comment]
-       var id: String?
-       
+    var mediaURL: URL
+    let mediaType: MediaType
+    let author: Author
+    let timestamp: Date
+    var comments: [Comment]
+    var id: String?
+    var latitude: Double?
+    var longitude: Double?
+    
+    
+    static private let mediaKey = "media"
+    static private let ratioKey = "ratio"
+    static private let mediaTypeKey = "mediaType"
+    static private let authorKey = "author"
+    static private let commentsKey = "comments"
+    static private let timestampKey = "timestamp"
+    static private let idKey = "id"
+    static private let longKey = "longitude"
+    static private let latKey = "latitude"
+    
     
     var title: String? {
         return comments.first?.text
     }
     
-    static private let mediaKey = "media"
-       static private let ratioKey = "ratio"
-       static private let mediaTypeKey = "mediaType"
-       static private let authorKey = "author"
-       static private let commentsKey = "comments"
-       static private let timestampKey = "timestamp"
-       static private let idKey = "id"
-    
-    
-    init(title: String, mediaURL: URL, ratio: CGFloat? = nil, author: Author, timestamp: Date = Date()) {
+    init(title: String, mediaURL: URL, ratio: CGFloat? = nil, author: Author, timestamp: Date = Date(), latitude: Double? = nil, longitude: Double? = nil ) {
         self.mediaURL = mediaURL
         self.mediaType = .video
         self.author = author
         self.comments = [Comment(text: title, author: author)]
         self.timestamp = timestamp
+        self.latitude = latitude
+        self.longitude = longitude
     }
     
     init?(dictionary: [String : Any], id: String) {
@@ -48,7 +54,10 @@ class VideoPost {
             let authorDictionary = dictionary[VideoPost.authorKey] as? [String: Any],
             let author = Author(dictionary: authorDictionary),
             let timestampTimeInterval = dictionary[VideoPost.timestampKey] as? TimeInterval,
-            let captionDictionaries = dictionary[VideoPost.commentsKey] as? [[String: Any]] else { return nil }
+            let captionDictionaries = dictionary[VideoPost.commentsKey] as? [[String: Any]],
+            let latitude = dictionary[VideoPost.latKey]  as? Double?,
+            let longitude = dictionary[VideoPost.longKey]  as? Double?  else { return nil }
+        
         
         self.mediaURL = mediaURL
         self.mediaType = mediaType
@@ -56,14 +65,18 @@ class VideoPost {
         self.timestamp = Date(timeIntervalSince1970: timestampTimeInterval)
         self.comments = captionDictionaries.compactMap({ Comment(dictionary: $0) })
         self.id = id
+        self.longitude = longitude
+        self.latitude = latitude
     }
-
+    
     var dictionaryRepresentation: [String : Any] {
         let dict: [String: Any] = [VideoPost.mediaKey: mediaURL.absoluteString,
-                VideoPost.mediaTypeKey: mediaType.rawValue,
-                VideoPost.commentsKey: comments.map({ $0.dictionaryRepresentation }),
-                VideoPost.authorKey: author.dictionaryRepresentation,
-                VideoPost.timestampKey: timestamp.timeIntervalSince1970]
+                                   VideoPost.mediaTypeKey: mediaType.rawValue,
+                                   VideoPost.commentsKey: comments.map({ $0.dictionaryRepresentation }),
+                                   VideoPost.authorKey: author.dictionaryRepresentation,
+                                   VideoPost.timestampKey: timestamp.timeIntervalSince1970,
+                                   VideoPost.latKey: latitude as Any,
+                                   VideoPost.longKey: longitude as Any]
         return dict
     }
     
